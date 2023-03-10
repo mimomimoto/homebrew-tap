7b30e6f22073ff944db91c4e34280bdd046cfa87
mimomimoto <99246880+mimomimoto@users.noreply.github.com>
2023-03-10 12:45:35 +0900

    Add files via upload

---
+++ Formula/colormkdir.rb
@@ -0,0 +1,17 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Colormkdir < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/mimomimoto/colormkdir/archive/refs/heads/main.zip"
+  version ""
+  sha256 ""
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "colormkdir"
+  end
+end
